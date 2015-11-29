c6ca438f018037039599a84273af8ede11c8b786
resteve <resteve@zikzakmedia.com>
2015-11-29 22:36:43 +0100

    Tests

--- tests/test_party_identifier.py
+++ tests/test_party_identifier.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
-# This file is part party_identifier module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the party_identifier module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, test_view,\
-    test_depends
-from trytond.transaction import Transaction
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PartyIdentifierTestCase(unittest.TestCase):
+class PartyIdentifierTestCase(ModuleTestCase):
     'Test Party Identifier module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('party_identifier')
-
-    def test0005views(self):
-        'Test views'
-        test_view('party_identifier')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'party_identifier'
 
 
 def suite():
@@ -29,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         PartyIdentifierTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
