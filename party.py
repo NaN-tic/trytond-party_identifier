1312e1e3fef96a7f9dd5fba25c27257906193af1
jmartin <jmartin@zikzakmedia.com>
2015-12-21 14:23:40 +0100

    Implements setter method for identifier_code field of party

--- party.py
+++ party.py
@@ -2,7 +2,7 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 
 __all__ = ['Party']
 __metaclass__ = PoolMeta
@@ -11,7 +11,8 @@ __metaclass__ = PoolMeta
 class Party:
     __name__ = 'party.party'
     identifier_code = fields.Function(fields.Char('Identifier Code'),
-        'get_identifier_code', searcher='search_identifier_code')
+        'get_identifier_code', setter='set_identifer_code',
+        searcher='search_identifier_code')
 
     def get_identifier_code(self, name):
         if self.identifiers:
@@ -23,3 +24,20 @@ class Party:
         return [
             ('identifiers.code',) + tuple(clause[1:]),
             ]
+
+    @classmethod
+    def set_identifer_code(cls, parties, name, value):
+        Identifier = Pool().get('party.identifier')
+        for party in parties:
+            identifier = Identifier.search([
+                    ('party', '=', party.id)
+                    ])
+            if identifier and value:
+                Identifier.write([identifier[0]], {'code': value})
+            elif identifier and not value:
+                Identifier.delete([identifier[0]])
+            else:
+                Identifier.create([{
+                            'party': party,
+                            'code': value,
+                            }])
